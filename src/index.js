9c63ee29fd225326b11ce0b4cc5fb7c3ade58fd2
onyangoouma <jacksonouma04@gmail.com>
2022-07-01 13:07:55 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) =>{
+        event.preventDefault()
+        const input = document.querySelector('input#searchByID');
+        // console.log(input.value);
+        // event.target.children('searchByID')
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data)
+            const title = document.querySelector('section#movieDetails h4')
+            const summary = document.querySelector('section#movieDetails p')
   
-}
-
-document.addEventListener('DOMContentLoaded', init);
+            //changing the content
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        })
+        // event.target.children[1].value
+    })
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
